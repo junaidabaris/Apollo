5aae9088cb380faab44f6eb1e092bfd8e95081d5
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2025-01-22 18:39:11 +0530

    fdvfd

---
+++ src/pages/engage/emails/index.js
@@ -0,0 +1,12 @@
+import Email from "../../../components/engage/emails/Email"
+
+
+const EmailPage = () => {
+    return (
+        <>
+            <Email />
+        </>
+    )
+}
+
+export default EmailPage
---
+++ src/pages/meetings/index.js
@@ -0,0 +1,12 @@
+import Meetings from "../../components/meetings/Meetings"
+
+
+const MeatingPage = () => {
+    return (
+        <>
+            <Meetings />
+        </>
+    )
+}
+
+export default MeatingPage
